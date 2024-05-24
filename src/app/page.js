7b4d8da59dbb3542643c13837ef96413eb87fc2b
pernwn <pernillepjohansen@hotmail.com>
2024-05-24 15:37:19 +0200

    -

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 
 
-import ClientRender from "./components/clientrender";
-import CircleCard from "./components/intro";
-import ExplainerCard from "./components/info";
-import Contact from "./components/contact";
-import Faq from "./components/faq";
-import Footer from "./components/footer";
-import Hero from './components/hero';
-import Benefits from './components/benefits';
+import ClientRender from "@/./components/clientrender";
+import CircleCard from "@/./components/intro";
+import ExplainerCard from "@/./components/info";
+import Contact from "@/./components/contact";
+import Faq from "@/./components/faq";
+import Footer from "@/./components/footer";
+import Hero from '@/./components/hero';
+import Benefits from '@/./components/benefits';
 
 
 export default function Page() {
